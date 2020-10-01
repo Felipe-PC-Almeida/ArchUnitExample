12cf9e88bb9f87aec8721a89a134b5807b52c643
Felipe-PC-Almeida <felipe.pc.almeida@gmail.com>
2020-10-01 10:08:09 -0300

    Commit do codigo contendo exemplo de como utilizar o ArchUnit

---
+++ src/main/java/br/edu/fas/business/Foo.java
@@ -0,0 +1,10 @@
+package br.edu.fas.business;
+
+//import br.edu.fas.persistence.Dao;
+//import br.edu.fas.persistence.FooDao;
+
+public class Foo /*implements Dao*/ {
+
+    //FooDao fooDao = new FooDao();
+
+}
